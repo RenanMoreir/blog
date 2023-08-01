2114779bec0544f518f12fb1c2166aed5c6241e8
Renan Moreira <renanrodriguesmoreira@hotmail.com>
2023-08-01 10:59:32 -0300

    fui-fui

---
+++ banco/banco_blog.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS blog;
+CREATE DATABASE blog;
+USE blog;
+
+create table usuario (
+	id int NOT NULL auto_increment,
+    nome varchar(255) not null,
+    senha varchar(60) not null,
+    data_criacao datetime not null default current_timestamp,
+    ativo tinyint not null default '0',
+    adm tinyint not null default '0',
+    primary key (id)
+);
+
+create table post (
+	id int not null auto_increment,
+    titulo varchar(255) not null,
+    texto text not null,
+    usuario_id int not null,
+    data_criacao datetime not null default current_timestamp,
+    data_postagem datetime not null,
+    primary key (id),
+    key fk_post_usuario_idx (usuario_id),
+    constraint fk_post_usuario foreign key (usuario_id) references usuario (id)
+ );
