7c187fd31c48be6b49c84f9e1c1dbfbe8962968e
Renan Moreira <renanrodriguesmoreira@hotmail.com>
2023-09-05 12:19:56 -0300

    lalalala

--- includes/topo.php
+++ includes/topo.php
@@ -6,6 +6,7 @@
     <div class="card-header">
         <h1>Projeto Blog em PHP + MySQL IFSP - MURILO</h1>
     </div>
+    
     <?php if(isset($_SESSION['login'])) : ?>
     <div class="card-body text-right">
         Olá <?php echo $_SESSION['login']['usuario']['nome'] ?>!
